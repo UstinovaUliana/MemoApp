83224f792c499e0a646a24af1e4388ef2b2d3be6
Uliana <mirrorrune21363@gmail.com>
2024-03-01 17:08:22 +0300

    add states

---
+++ app/src/main/java/com/example/memoapp/model/MusicState.kt
@@ -0,0 +1,8 @@
+package com.example.memoapp.model
+
+enum class MusicState {
+    PLAY,
+    PAUSE,
+    STOP,
+    SHUFFLE_SONGS
+}
--- app/src/main/java/com/example/memoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/memoapp/ui/MainActivity.kt
@@ -5,13 +5,17 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.*
+import android.view.View
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.memoapp.R
 import com.example.memoapp.databinding.ActivityMainBinding
 import com.example.memoapp.helper.SharedPrefs
+import com.example.memoapp.model.Level
+import com.example.memoapp.model.Level.Companion.getLevel
 import com.example.memoapp.ui.adapter.IconAdapter
 import com.example.memoapp.viewmodel.MainViewModel
 
@@ -32,7 +36,7 @@ class MainActivity : AppCompatActivity() {
     private val alertBuilder by lazy { AlertDialog.Builder(this) }
     private val handler by lazy { Handler(Looper.getMainLooper()) }
 
-    private val timereReceiver: TimerReciever by lazy { TimerReceiver() }
+    private val timereReceiver: TimerReceiver by lazy { TimerReceiver() }
 
     inner class TimerReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
@@ -113,7 +117,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun sendCommandToForegroundService(state: MusicState) {
+    private fun sendCommandToBoundervice(state: MusicState) {
         if (mainViewModel.isMusicServiceBound) {
 
             informUser(state)
@@ -133,6 +137,170 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
+    private fun sendCommandToForegroundService(timerState: TimerState) {
+        mainViewModel.isForegroundServiceRunning = timerState != TimerState.STOP
+    }
 
+    private fun getServiceIntent(command: TimerState) =
+        Intent(this, TimerService::class.java).apply{
+            putExtra(SERVICE_COMMAND, command as Parcelable)
+        }
+
+
+    private fun setupUi() {
+        with(binding) {
+            gridview.adapter = adapter
+            btnPlay.onClick {
+                prepareCardView()
+                sendCommandToForegroundService(TimerState.START)
+            }
+            btnPlayMusic.onClick {
+                sendCommandToBoundervice(MusicState.PLAY)
+            }
+            btnPauseMusic.onClick {
+                sendCommandToBoundervice(MusicState.PAUSE)
+            }
+            btnStopMusic.onClick {
+                sendCommandToBoundervice(MusicState.STOP)
+            }
+            btnShuffleMusic.onClick {
+                getNameOfSong()
+            }
+            btnQuit.onClick {
+                binding.tvTime.clearComposingText()
+                sendCommandToForegroundService(TimerState.STOP)
+            }
+        }
+
+        updateVisibility()
+    }
+
+    private fun updateVisibility() {
+        with (binding) {
+            val btnPlayVisible = if (mainViewModel.isForegroundServiceRunning) {
+                View.INVISIBLE
+            } else {
+                View.VISIBLE
+            }
+
+            val gridVisible = if (mainViewModel.isForegroundServiceRunning) {
+                View.VISIBLE
+            } else {
+                View.INVISIBLE
+            }
+            btnPlay.apply {
+                visibility = btnPlayVisible
+                text = String.format(
+                    getString(R.string.play_p_level),
+                    getLevel(sharedPrefs.getStoredLevel())?.name
+                )
+            }
+            gridview.visibility = gridVisible
+            tvTime.visibility = gridVisible
+            btnQuit.visibility = gridVisible
+        }
+    }
+
+    private fun prepareCardView () {
+        val currentLevel = getLevel(sharedPrefs.getStoredLevel()) ?: Level.BEGINNER
+
+        mainViewModel.pairs = 0
+        mainViewModel.pairsSum = currentLevel.numberOfCards
+
+        binding.gridview.numColumns = mainViewModel.getNumberOfColumns(currentLevel)
+
+        adapter.updateData(mainViewModel.gerRandomItems(sharedPrefs.getStoredLevel()))
+    }
+
+    private fun enableButtons(state: MusicState) {
+        val songPlays = state == MusicState.PLAY || MusicState.SHUFFLE_SONGS
+        with(binding) {
+            btnPlayMusic.isEnabled = !songPlays
+            btnPauseMusic.isEnabled = songPlays
+            btnStopMusic.isEnabled = songPlays
+            btnShuffleMusic.isEnabled = songPlays
+            btnSongName.apply {
+                isEnabled = songPlays
+                btnSongName.visibility = if (songPlays) {
+                    View.VISIBLE
+                } else {
+                    View.INVISIBLE
+                }
+            }
+        }
+    }
+
+    private fun updateUi(elapsedTime: Int) {
+        mainViewModel.elapsedTime = elapsedTime
+        binding.tvTime.text = elapsedTime.secondsToTime()
+    }
+
+    fun checkProgress() {
+        val currentLevel = getLevel(sharedPrefs.getStoredLevel())
+        if (currentLevel == Level.NONE) {
+            showResetProgressDialog()
+        }
+    }
+
+    private fun storeLevel() {
+        val currentLevel = getLevel(sharedPrefs.getStoredLevel())
+        currentLevel?.let {
+            sharedPrefs.storeLevel(
+                when(it) {
+                    Level.BEGINNER -> Level.INTERMEDIATE
+                    Level.INTERMEDIATE -> Level.ADVANCED
+                    Level.ADVANCED -> Level.EXPERT
+                    Level.EXPERT -> Level.NONE
+                    Level.NONE -> Level.BEGINNER
+                }
+            )
+        }
+    }
+
+    private fun informUser(state: MusicState) {
+        @StringRes val res = when (state) {
+            MusicState.PLAY -> R.string.music_started
+            MusicState.PAUSE -> R.string.music_paused
+            MusicState.STOP -> R.string.music_stopped
+            MusicState.SHUFFLE_SONGS -> R.string.songs_shuffled
+
+        }
+
+        Toast.makeText(this, res, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showSuccessDialog() {
+        showDialog(
+            String.format(getString(R.string.well_done_your_time_is_p), mainViewModel.elapsedTime),
+            getString(R.string.click_ok_for_proceeding_to_the_next_level),
+        ) {
+            checkProgress()
+        }
+    }
+
+    private fun showResetProgressDialog() {
+        showDialog(
+            getString(R.string.you_have_finished_all_levels),
+            getString(R.string.click_ok_to_reset_progress),
+        ) {
+            sharedPrefs.storeLevel(Level.BEGINNER)
+        }
+    }
+
+    private fun showDialog(
+        title: String,
+        message: String,
+        action: () -> Unit
+    ) {
+        with(alertBuilder) {
+            setCancelable(false)
+            setTitle(title)
+            setMessage(message)
+            setPositiveButton(getString(R.string.ok)) { _, _ ->
+                action()
+            }
+            show()
+        }
+    }
 
 }
